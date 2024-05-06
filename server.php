cafc195254ac063d0ad8afd903df98a3c6b6120f
filippo-falcone <falconefilippo98@hotmail.com>
2024-05-06 14:47:39 +0200

    feat: added $response to server.php and manual comments

--- server.php
+++ server.php
@@ -1,4 +1,15 @@
 <?php
+/* API Manual */
+// Query params:
+// disc => indice che rappresenta il singolo oggetto ammette interi da 0 a 5
+// ese: disc = 0 response {
+//     "title": "New Jersey",
+//     "author": "Bon Jovi",
+//     "year": 1988,
+//     "poster": "https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg",
+//     "genre": "Rock"
+// }
+
 $discs = [
     [
         "title" => "New Jersey",
@@ -44,6 +55,14 @@ $discs = [
     ]
 ];
 
+/* response */
+$response = $discs;
+if(isset($_GET['disc'])) {
+    $disc = intval($_GET['disc']);
+    $response = $discs[$disc];
+}
+
+/* output */
 header('Content-Type: application/json');
 echo json_encode($response);
 ?>
